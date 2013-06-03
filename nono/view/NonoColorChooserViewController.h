b8ee82443d630b49260ddf362b1aa92667243108
Charles Francoise <charles.francoise@gmail.com>
2013-06-03 23:09:36 +0200

    start work on color chooser

---
+++ nono/view/NonoColorChooserViewController.h
@@ -0,0 +1,13 @@
+//
+//  NonoColorChooserViewController.h
+//  nono
+//
+//  Created by Charles Francoise on 6/3/13.
+//  Copyright (c) 2013 Charles Francoise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NonoColorChooserViewController : UIViewController
+
+@end
---
+++ nono/view/NonoColorChooserViewController.m
@@ -0,0 +1,38 @@
+//
+//  NonoColorChooserViewController.m
+//  nono
+//
+//  Created by Charles Francoise on 6/3/13.
+//  Copyright (c) 2013 Charles Francoise. All rights reserved.
+//
+
+#import "NonoColorChooserViewController.h"
+
+@interface NonoColorChooserViewController ()
+
+@end
+
+@implementation NonoColorChooserViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
